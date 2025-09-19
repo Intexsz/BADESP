9d68240c31312107dd217c27fadab192f1beeab4
Manoel José da Silva Junior <juniorsolva2017@gmail.com>
2025-09-19 17:13:38 -0300

    arrumei o ajuda não ser totalmente clicavel

--- static/js/Ajuda.js
+++ static/js/Ajuda.js
@@ -6,21 +6,15 @@ menuBtn.addEventListener('click', () => {
     sidebar.classList.toggle('active');
 });
 
-// ACORDEÃO FAQ
-const faqs = document.querySelectorAll('#ajuda .faq');
+const faqs = document.querySelectorAll('.painel .faq');
 
 faqs.forEach(faq => {
-    const pergunta = faq.querySelector('h3');
-
-    pergunta.addEventListener('click', () => {
-        // Fecha outras FAQs abertas
-        faqs.forEach(f => {
-            if (f !== faq) {
-                f.classList.remove('active');
-            }
-        });
-
-        // Alterna a FAQ clicada
-        faq.classList.toggle('active');
+  faq.addEventListener('click', () => {
+    faqs.forEach(f => {
+      if (f !== faq) f.classList.remove('active');
     });
+    faq.classList.toggle('active');
+  });
 });
+
+
