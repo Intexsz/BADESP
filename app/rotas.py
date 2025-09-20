827e8031862dd8d77810959fd2585691d5dcf9af
Manoel José da Silva Junior <juniorsolva2017@gmail.com>
2025-09-20 00:58:33 -0300

    implementei o status, quando o status da denuncia é diferente de 'Em analise.' ou 'Expirada.' o aluno não consegue mais excluir ela pois a secretaria abriu a denuncia, arquivou ou aprovou

--- app/rotas.py
+++ app/rotas.py
@@ -1,9 +1,7 @@
-from flask import Flask, request, jsonify, render_template, session, redirect, url_for, Blueprint
-from google.oauth2 import id_token
-from google.auth.transport import requests
+from flask import Flask, request, render_template, session, redirect, url_for, Blueprint
 from authlib.integrations.flask_client import OAuth
-from app.bancodedados import salvar_usuario, buscar_usuario
-from app.bancodedados import criar_denuncia, mostrar_denuncias, apagar_denuncia
+from app.bancodedados import buscar_usuario, buscar_status_denuncia
+from app.bancodedados import criar_denuncia, mostrar_denuncias, apagar_denuncia, expirar
 
 app = Flask(__name__)
 rotas_bp = Blueprint('rotas', __name__)
@@ -21,56 +19,11 @@ google = oauth.register(
     client_kwargs={'scope': 'openid email profile'},
 )
 
-'''###### LOGIN ######
-@rotas_bp.route('/')
-def homepage():
-    if "user_id" in session:
-        return redirect(url_for('rotas.inicio'))
-    return redirect(url_for('rotas.cadastro'))
-
-@rotas_bp.route('/Login/callback', methods=["POST", "GET"])
-def callback():
-    token = None
-    if request.is_json:
-        token = request.json.get("credential")
-    elif "credential" in request.form:
-        token = request.form.get("credential")
-    elif "credential" in request.args:
-        token = request.args.get("credential")
-
-    if not token:
-        return jsonify({"error": "Token não fornecido"}), 400
-
-    try:
-        idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
-        user_data = {
-            "id": idinfo["sub"],
-            "email": idinfo["email"],
-            "name": idinfo.get("name"),
-            "picture": idinfo.get("picture")
-        }
-        salvar_usuario(user_data)
-        session["user_id"] = user_data["id"]
-        return jsonify({"status": "ok", "user": user_data})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 400
-
-@rotas_bp.route('/Logout')
-def logout():
-    session.clear()
-    return redirect(url_for("rotas.cadastro"))
-
-@rotas_bp.route('/Login', methods=['GET', 'POST'])
-def cadastro():
-    if "user_id" in session:
-        return redirect(url_for('rotas.inicio'))
-    return render_template('login.html')
-'''
-
 ###### PAGINA NORMAL ######
 @rotas_bp.route('/Inicio')
 def inicio():
     if "user_id" in session:
+        expirar()
         usuario = buscar_usuario(session["user_id"])
         if usuario:
             denuncias = mostrar_denuncias(session["user_id"])
@@ -98,7 +51,7 @@ def denuncia():
         gravidade = request.form.get('gravidade')
         descricao = request.form.get('descricao')
 
-        criar_denuncia(titulo, gravidade, descricao, session["user_id"])
+        criar_denuncia(titulo, gravidade, descricao, session["user_id"], 'Em Análise.')
 
     return render_template('denuncia.html')
 
@@ -106,6 +59,19 @@ def denuncia():
 def excluir_denuncia(id):
     if "user_id" not in session:
         return redirect(url_for('rotalogin.cadastro'))
+    
+    status = buscar_status_denuncia(id, session["user_id"])
+    if status == 'Em Análise.' or status == 'Expirada.':
+        apagar_denuncia(id, session["user_id"])
+        return redirect(url_for('rotas.inicio'))
+    else:
+        return f"""
+            <script>
+                alert("Não é mais possível deletar denúncia.");
+                window.location.href = "{url_for('rotas.inicio')}";
+            </script>
+        """
 
-    apagar_denuncia(id, session["user_id"])
-    return redirect(url_for('rotas.inicio'))
+    
+    
+
